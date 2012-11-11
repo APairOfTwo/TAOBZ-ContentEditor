b4180b83a067f88961b0d6cb12909731682b165d
Guilherme Ouriques <guilherme.nro@gmail.com>
2012-11-11 20:04:19 -0200

    Teclas de scroll do mapa alteradas
    WASD ao invés de setas para movimentar pelo mapa.

--- src/KeyboardInput.java
+++ src/KeyboardInput.java
@@ -21,10 +21,10 @@ public class KeyboardInput implements KeyListener {
 
 	public void keyPressed(KeyEvent e) {
 		int intKeyCode = e.getKeyCode();
-		if (intKeyCode == KeyEvent.VK_UP)		UP			= true;
-		if (intKeyCode == KeyEvent.VK_DOWN)		DOWN		= true;
-		if (intKeyCode == KeyEvent.VK_LEFT)		LEFT		= true;
-		if (intKeyCode == KeyEvent.VK_RIGHT)	RIGHT		= true;
+		if (intKeyCode == KeyEvent.VK_W)		UP			= true;
+		if (intKeyCode == KeyEvent.VK_S)		DOWN		= true;
+		if (intKeyCode == KeyEvent.VK_A)		LEFT		= true;
+		if (intKeyCode == KeyEvent.VK_D)		RIGHT		= true;
 		if (intKeyCode == KeyEvent.VK_SPACE)	SPACE		= true;
 		if (intKeyCode == KeyEvent.VK_ENTER)	ENTER		= true;
 		if (intKeyCode == KeyEvent.VK_ESCAPE)	ESCAPE		= true;
@@ -39,10 +39,10 @@ public class KeyboardInput implements KeyListener {
 
 	public void keyReleased(KeyEvent e) {
 		int intKeyCode = e.getKeyCode();
-		if (intKeyCode == KeyEvent.VK_UP)		UP			= false;
-		if (intKeyCode == KeyEvent.VK_DOWN)		DOWN		= false;
-		if (intKeyCode == KeyEvent.VK_LEFT)		LEFT		= false;
-		if (intKeyCode == KeyEvent.VK_RIGHT)	RIGHT		= false;
+		if (intKeyCode == KeyEvent.VK_W)		UP			= false;
+		if (intKeyCode == KeyEvent.VK_S)		DOWN		= false;
+		if (intKeyCode == KeyEvent.VK_A)		LEFT		= false;
+		if (intKeyCode == KeyEvent.VK_D)		RIGHT		= false;
 		if (intKeyCode == KeyEvent.VK_SPACE)	SPACE		= false;
 		if (intKeyCode == KeyEvent.VK_ENTER)	ENTER		= false;
 		if (intKeyCode == KeyEvent.VK_ESCAPE)	ESCAPE		= false;
